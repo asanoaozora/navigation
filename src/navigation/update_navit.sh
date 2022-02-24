37b07008de185fb398ae33338c8670aef6d99e94
asanoaozora <asanoaozora@outlook.com>
2022-02-24 18:07:48 +0100

    add another patch to fix inkscape issue

--- src/navigation/update_navit.sh
+++ src/navigation/update_navit.sh
@@ -18,7 +18,8 @@ git checkout $navit_version
 patch -p0 -i ../patches/search_list_get_unique.diff
 patch -p0 -i ../patches/fsa_issue_padding.diff
 patch -p0 -i ../patches/avoid-crash-on-guidance-when-delete-and-recreate-route.diff
-patch -p0 -i ../patches/inkscape_wrong_option.diff
+patch -p0 -i ../patches/inkscape_wrong_option-icons.diff
+patch -p0 -i ../patches/inkscape_wrong_option-textures.diff
 cd ../
 
 
